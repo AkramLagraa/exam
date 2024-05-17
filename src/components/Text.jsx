d0af83438319fa55a090d3208276f446769c683a
akram <aargalmarka@gmail.com>
2024-05-17 15:10:27 -0400

    exam

---
+++ src/components/Text.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Text() {
+  return (
+    <div className="absolute flex flex-col text-[5.5vw] uppercase w-[80vw] items-start leading-tight">
+      <p>Refaction Scientifique</p>
+      <p className="self-end">W Startup</p>
+      <p>Jaw harder than</p>
+      <p className="self-end">Dss , Security And Ai</p>
+    </div>
+  )
+}
